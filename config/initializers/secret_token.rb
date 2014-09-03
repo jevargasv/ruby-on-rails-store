db32e25e5c78d46cef2086a2a001858ac4a14483
Joe Prempeh <prempehjoe@gmail.com>
2014-09-03 14:25:56 -0700

    Store Scaffold

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Store::Application.config.secret_key_base = 'a91cd63745f4832ba1ccd41967d136cb9b29d6825bd12c4af4aa2264b0f30ab2938b2c91533e75dd8c377b55ae95bd66b6a5c570d58df9a4c70c09c825ad23e9'
